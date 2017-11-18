1e85be407faace3a61d46e95d88d04f4b0950123
xiw5 <1404861473@qq.com>
2017-11-18 17:19:27 +0000

    done

---
+++ lab_set.hpp
@@ -0,0 +1,385 @@
+#ifndef LAB_SET_HPP_
+#define LAB_SET_HPP_
+
+#include "labafx.hpp"
+#include<vector>
+// DO NOT MODIFY BELOW
+
+namespace Lab {
+template <typename T> class set {
+public:
+	struct node
+	{
+		bool color;
+		node *(tr[2]);
+		node *fa;
+		//int number;
+		node *pre;
+		node *next;
+		T value;
+	};
+	//std::vector<node> tree;
+  set()
+	{
+    sum=0;
+		root=NULL;
+	} 
+  int size() const
+	{
+		return sum;
+	}
+	void rotate(node *x)
+	{
+	  node *y=x->fa;
+		node *z=y->fa;
+    int l;
+		int r;
+		if(y->tr[0]==x)
+			l=0;
+		else
+			l=1;
+		r=l^1;
+		if(y==root)
+			root=x;
+		else
+		{
+			if(z->tr[0]==y)
+				z->tr[0]=x;
+			else
+				z->tr[1]=x;
+		}
+		x->fa=z;
+		y->fa=x;
+		if(x->tr[r]!=NULL)
+		  x->tr[r]->fa=y;
+		y->tr[l]=x->tr[r];
+		x->tr[r]=y;
+	}
+	void insertupdata(node *x)// can alter
+	{
+    for(;x->fa!=NULL&&x->fa->color==0;)
+		{
+			node *y=x->fa;
+			node *z=y->fa;
+			if(y==z->tr[0])
+			{
+				if(z->tr[1]!=NULL&&z->tr[1]->color==0)
+				{
+					z->tr[1]->color=1;
+					y->color=1;
+					z->color=0;
+					x=z;
+					continue;
+				}
+				if(y->tr[1]==x)
+				{
+					rotate(x);
+					std::swap(x,y);
+				}
+				y->color=1;
+				z->color=0;
+				rotate(y);
+			}
+			else
+			{
+				if(z->tr[0]!=NULL&&z->tr[0]->color==0)
+				{
+					z->tr[0]->color=1;
+					y->color=1;
+					z->color=0;
+					x=z;
+					continue;
+				}
+				if(y->tr[0]==x)
+				{
+					rotate(x);
+					std::swap(x,y);
+				}
+				y->color=1;
+				z->color=0;
+				rotate(y);
+      }
+		}
+    root->color=1;
+	}
+  void insert(const T &item)
+	{
+		//std::cout<<"1 "<<item<<std::endl;
+		T max=item;
+		node *maxx=NULL;
+		T min=item;
+		node *minn=NULL;
+    node *newnode=new node;
+    newnode->color=0;
+		newnode->tr[0]=NULL;
+		newnode->tr[1]=NULL;
+		newnode->fa=NULL;
+		newnode->pre=NULL;
+		newnode->next=NULL;
+		newnode->value=item;
+		//newnode.number=sum;
+		//tree.push_back(newnode);
+		if(root==NULL)
+      root=newnode;
+		else
+		{
+			node *x=root;
+			node *y;
+			for(;x!=NULL;)
+			{
+				y=x;
+				if(x->value>item)
+				{
+					if(x->value<max||max==item)
+					{
+						max=x->value;
+						maxx=x;
+					}
+					x=x->tr[0];
+				}
+				else
+				{
+					if(x->value>min||min==item)
+					{
+						min=x->value;
+						minn=x;
+					}
+					x=x->tr[1];
+				}
+			}
+			newnode->fa=y;
+      if(y->value>item)
+				y->tr[0]=newnode;
+			else
+				y->tr[1]=newnode;
+	    if(maxx!=NULL)
+			{
+				newnode->next=maxx;
+				maxx->pre=newnode;
+			}
+			if(minn!=NULL)
+			{
+				newnode->pre=minn;
+				minn->next=newnode;
+			}
+	  }
+		insertupdata(newnode);
+    sum++;
+	}
+  class iterator {
+  public:
+    const T &operator*()
+		{
+			return t->value;
+		}
+		node* ret()
+	  {
+			return t;
+		}
+		iterator()
+		{
+			t=NULL;
+		}
+		iterator(node *tt)
+		{
+			t=tt;
+		}	
+		iterator& operator++()
+		{
+			t=t->next;
+			return *this;
+		}
+		iterator& operator--()
+		{
+			t=t->pre;
+			return *this;
+		}
+    iterator operator++(int)	// for iter++
+		{
+			iterator iter=this;
+			++this;
+			return iter;
+		}
+    iterator operator--(int)
+		{
+		  iterator iter=this;
+			--this;
+			return iter;
+		}
+    bool operator==(const iterator &another) const
+		{
+			return t==another.t;
+		}
+    bool operator!=(const iterator &another) const
+		{
+			return t!=another.t;
+		}
+  private:
+    node *t;
+    // add your data and function here
+  };
+  iterator find(const T &item)
+	{
+		node *x=root;
+		for(;x->value!=item;)
+		{
+			if(x->value>item)
+				x=x->tr[0];
+			else
+				x=x->tr[1];
+		}
+		return iterator(x);
+	}
+	void eraseupdata(node *x,node *father)
+	{
+		node *brother;
+    for(;(x==NULL||x->color==1)&&x!=root;)
+	    {
+				if(father->tr[0]==x)
+				{
+				  brother=father->tr[1];
+					if(brother->color==0)
+					{
+						brother->color=1;
+						father->color=0;
+						rotate(brother);
+						brother=father->tr[1];
+					}
+					if((brother->tr[0]==NULL||brother->tr[0]->color)&&(brother->tr[1]==NULL||brother->tr[1]->color))
+					{
+						brother->color=0;
+						x=father;
+						father=x->fa;
+					}
+					else
+					{
+						if(brother->tr[1]==NULL||brother->tr[1]->color)
+						{
+							brother->tr[0]->color=1;
+							brother->color=0;
+							rotate(brother->tr[0]);
+							brother=father->tr[1];
+						}
+						brother->color=father->color;
+						father->color=1;
+						brother->tr[1]->color=1;
+						rotate(brother);
+						x=root;
+						break;
+					}
+				}
+				else
+				{
+					brother=father->tr[0];
+					if(brother->color==0)
+					{
+						brother->color=1;
+						father->color=0;
+						rotate(brother);
+						brother=father->tr[0];
+					}
+          if((brother->tr[0]==NULL||brother->tr[0]->color)&&(brother->tr[1]==NULL||brother->tr[1]->color))
+					{
+            brother->color=0;
+						x=father;
+						father=x->fa;
+					}
+					else
+					{
+						if(brother->tr[0]==NULL||brother->tr[0]->color)
+						{
+							brother->tr[1]->color=1;
+							brother->color=0;
+							rotate(brother->tr[1]);
+							brother=father->tr[0];
+						}
+						brother->color=father->color;
+						father->color=1;
+						brother->tr[0]->color=1;
+						rotate(brother);
+						x=root;
+						break;
+					}
+				}
+			}
+		if(x!=NULL)
+			x->color=1;
+	}
+  void erase(iterator it)
+	{
+	//	printf("-1 ");
+	  sum--;
+		node *x=it.ret();
+		node *del=x;
+		node *child;
+		if(x->tr[0]!=NULL&&x->tr[1]!=NULL)
+      {
+				del=x->tr[0];
+				for(;del->tr[1]!=NULL;del=del->tr[1]);
+			}
+    if(del->tr[0]==NULL)
+			child=del->tr[1];
+		else 
+			child=del->tr[0];
+	 if(child!=NULL)
+		 child->fa=del->fa;
+	 if(del->fa==NULL)
+		 root=child;
+	 else  if(del==del->fa->tr[0])
+		       del->fa->tr[0]=child;
+	       else
+					 del->fa->tr[1]=child;
+	 if(del!=x)
+     x->value=del->value;
+	 if(del->pre!=NULL)
+		 del->pre->next=del->next;
+	 if(del->next!=NULL)
+		 del->next->pre=del->pre;
+   if(del->color==1)
+		 eraseupdata(child,del->fa);
+	}
+  iterator begin()
+	{
+		node *x=root;
+		if(root==NULL)
+      return iterator();
+	//	printf("%d %d\n",x,sum);
+		for(;x->tr[0]!=NULL;x=x->tr[0]);
+		return iterator(x);
+	}
+  iterator end()//all trouble???????
+	{
+		if(root==NULL)
+			return iterator();
+		node *x=root;
+		for(;x->tr[1]!=NULL;x=x->tr[1]);
+		return iterator(x);
+	}
+	void nclear(node *x)
+	{
+		if(x==NULL)
+			return;
+    nclear(x->tr[0]);
+		nclear(x->tr[1]);
+		delete x;
+		return;
+	}
+  void clear()
+	{
+		sum=0;
+		node *x=root;
+		nclear(x);
+    root=NULL;
+  }
+  ~set()
+	{
+		clear();
+	}
+
+private:
+	int sum;
+	node *root;
+  // add your data and function here
+};
+}
+#endif
---
+++ lab_unordered_map.hpp
@@ -0,0 +1,178 @@
+#ifndef LAB_UNORDERED_MAP_HPP_
+#define LAB_UNORDERED_MAP_HPP_
+#define SIZE  701819
+#include "labafx.hpp"
+namespace Lab {
+template <typename Key, typename Value, typename Hash = hash<Key>
+          /*, typename KeyEqual=equal_to<Key> */>
+// KeyEqual is not required by zht. Make him happy.
+class unordered_map {
+public:
+	struct node
+	{
+		::std::pair<const Key,Value> data;
+		node *next;
+		node *pre;
+		node *Next;
+		node *Pre;
+	};
+	node *Begin=NULL;
+	node *End=NULL;
+	node *later;
+  node **group;
+  unordered_map()
+	{
+		sum=0;
+    group=new node*[SIZE];
+	}
+  size_t size() const
+	{
+		return size_t(sum);
+	}
+  int hash_(Key KEy)
+	{
+		int Has=Hash()(KEy)%SIZE;
+		return Has;
+	}
+  void insert(::std::pair<Key, Value> kvpair)
+	{
+    int Ha=hash_(kvpair.first);
+    node* newnode=new node{kvpair,NULL,NULL,NULL,NULL};
+		if(Begin==NULL)
+			Begin=newnode;
+		End=newnode;
+		newnode->Pre=later;
+		newnode->Next=NULL;
+		later->Next=newnode;
+		later=newnode;
+		newnode->next=group[Ha];
+		newnode->pre=NULL;
+		if(group[Ha])
+			group[Ha]->pre=newnode;
+		group[Ha]=newnode;
+		sum++;
+	}
+ /* static node *add(node *t) 
+	{
+     int Ha=hash_(t->data.first);
+		 for(Ha=(Ha+1)%SIZE;group[Ha];Ha=(Ha+1)%SIZE);
+		 return group[Ha];					
+	}*/
+  class iterator {
+  public:
+    // assume Key won't be modified
+    // actually priviledge controling is hard
+    // since zht doesn't require it. Make him happy
+		iterator(node *tt)
+		{
+			t=tt;
+		}
+    ::std::pair<const Key, Value> &operator*()
+		{
+		  return t->data;
+		}
+    // prefix increment operator is not required by zht. Make him happy
+    // NO.
+    iterator &operator++()
+		{
+			t=t->Next;
+			return *this;
+		}
+    iterator &operator--()
+		{
+			if(t->Pre)
+				t=t->Pre;
+			return *this;
+		}
+	 	iterator operator++(int)
+		{
+			iterator iter=*this;
+			++(*this);
+			return iter;
+		}
+    iterator operator--(int)
+		{
+			iterator iter=*this;
+			--(*this);
+			return iter;
+		}
+    bool operator==(const iterator &another) const
+		{
+			return t==another.t;
+		}
+    bool operator!=(const iterator &another) const
+		{
+			return t!=another.t;
+		}
+		node * self()
+		{
+			return t;
+		}
+  private:
+		node *t;
+    // add your data and function here
+  };
+  iterator begin()
+	{
+		return iterator(Begin);
+	}
+  iterator end()
+	{
+		return iterator(NULL);
+	}
+  iterator find(const Key &item)
+	{
+    int Ha=hash_(item);
+		node* tt=group[Ha];
+		for(;tt->data.first!=item;tt=tt->next);
+		return iterator(tt);
+	}
+  void erase(iterator iter)
+	{
+    node *tt=iter.self();
+		if(tt->Pre)
+		  tt->Pre->Next=tt->Next;
+		else
+		  Begin=tt->Next;
+	  if(tt->Next)
+		  tt->Next->Pre=tt->Pre;
+	  else
+		  End=tt->Pre;	
+		int Ha=hash_(tt->data.first);
+		if(tt->pre)
+		{
+			tt->pre->next=tt->next;
+			if(tt->next)
+				tt->next->pre=tt->pre;
+		}
+		else
+		{
+			group[Ha]=tt->next;
+			if(tt->next)
+				tt->next->pre=NULL;
+		}	
+		delete tt;
+		sum--;
+	}
+	void clear()
+  {
+	/*	for(;Begin!=NULL;)
+		{
+			node *x=Begin;
+			Begin=Begin->Next;
+			delete x;
+		}*/
+	//	delete[] group;
+		sum=0;
+    Begin=NULL;
+  }
+  ~unordered_map()
+  {
+		clear();
+	}
+private:
+	int sum;
+  // add your data and function here
+};
+}
+#endif
